85a88340a8b0f68dca161ca243ea998d3866e90b
nastyabess <77615212+nastyabess@users.noreply.github.com>
2023-05-02 16:31:30 +0300

    тренажернрый зал

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {setupVideo} from './modules/video';
 
 // ---------------------------------
 
@@ -13,6 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modules
   // ---------------------------------
+  setupVideo();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
---
+++ source/js/modules/video.js
@@ -0,0 +1,27 @@
+const URL = 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1';
+
+const setupVideo = () => {
+
+  const video = document.querySelector('.video');
+  const media = video.querySelector('.video__media');
+  const button = video.querySelector('.video__button');
+
+
+  button.addEventListener('click', () => {
+    let iframe = createIframe();
+    media.remove();
+    button.remove();
+    video.appendChild(iframe);
+  });
+};
+
+const createIframe = () => {
+  let iframe = document.createElement('iframe');
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('src', URL);
+
+  return iframe;
+};
+
+export {setupVideo};
+
